c2d904917be2cdaed9c0d29991dc1f0d7ce4e925
Paul Kruse <paul.kruse@posteo.de>
2020-03-19 11:59:09 +0100

    Finished basic skin based on the old one

--- includes/NordlichtTemplate.php
+++ includes/NordlichtTemplate.php
@@ -22,7 +22,7 @@ class NordlichtTemplate extends BaseTemplate {
                 $this->getRightSidebar()).
             $this->getMyFooter();
 
-        $html .= Html::rawElement( 'div', ["class" => "container"],
+        $html .= Html::rawElement( 'div', ["class" => "container-lg"],
             Html::rawElement( 'div', ["class" => "content"] ,$containerData));
 
 
@@ -35,7 +35,20 @@ class NordlichtTemplate extends BaseTemplate {
 
     private function getHeader()
     {
-        $html = Html::rawElement("div", ["class" => "head"], "Test");
+        $head = "";
+        if ($this->data['loggedin']) {
+            $list = "";
+            foreach ($this->data['content_actions'] as $key => $tab) {
+                $list .= $this->makeListItem( $key, $tab );
+
+            }
+            $head .= Html::rawElement("ul", [], $list);
+        }
+        $html = Html::rawElement("div", ["class" => "row"],
+            Html::rawElement("div", ["class" => "col"],
+                Html::rawElement("div", ["class" => "head"], $head)
+            )
+        );
         return $html;
     }
 
@@ -45,31 +58,74 @@ class NordlichtTemplate extends BaseTemplate {
         foreach ( $this->getSidebar() as $boxName => $box ) {
             $sidebar .= Html::rawElement("div", ["id" => Sanitizer::escapeId( $box['id'])]);
             $sidebar .= Html::rawElement("h5", [], htmlspecialchars( $box['header'] ));
-        }
-        $items = "";
-        foreach ( $box['content'] as $key => $item ) {
-            $items .= $this->makeListItem( $key, $item );
-        }
-        if ( is_array( $box['content'] ) ) {
-            $sidebar .= Html::rawElement("ul", [], $items);
-        } else {
-            $sidebar .= $box['content'];
+            if ( is_array( $box['content'] ) ) {
+                $items = "";
+                foreach ( $box['content'] as $key => $item ) {
+                    $items .= $this->makeListItem( $key, $item );
+                }
+                $sidebar .= Html::rawElement("ul", [], $items);
+            } else {
+                $sidebar .= $box['content'];
+            }
         }
         return Html::rawElement("div", ["class"=>"col-2 sidebar"], $sidebar);
     }
 
     private function getContent()
     {
-        return Html::rawElement("div", ["class"=>"col main"], "Cont");
+        $content = "";
+        $content .= $this->get('sitenotice');
+        $content .= Html::rawElement("h1", ["class" => "firstHeadding"],
+        $this->data['displaytitle']!="" ? $this->get('title') : $this->get('title'));
+        Html::rawElement("div", ["id"=>"contentSub"], $this->get('subtitle'));
+
+        if($this->data['undelete']) { $content .= Html::rawElement("div",  ["id"=>"contentSub2"], $this->get('undelete'));}
+        if($this->data['newtalk']) { $content .= Html::rawElement("div",  ["class"=>"usermessage"], $this->get('newtalk'));}
+
+        $content .= $this->get('bodytext');
+        $content .= $this->get('catlinks');
+        return Html::rawElement("div", ["class"=>"col main"], $content);
     }
 
     private function getRightSidebar()
     {
-        return Html::rawElement("div", ["class"=>"col-2"], "Right");
+        $titel = Html::rawElement("h5", [],
+            Html::rawElement("label", ["for"=>"searchInput"],  "Suche")
+        );
+        $form = Html::rawElement("form", ["action" => $this->get( 'wgScript' ), "id"=>"searchform"],
+                Html::rawElement("input", ["type"=>"hidden", "value" => $this->get( 'searchtitle' )]).
+                $this->makeSearchInput().
+                $this->makeSearchButton( 'go' ).
+                $this->makeSearchButton( 'fulltext' )
+                );
+
+        $personalLinks = "";
+        foreach($this->getPersonalTools() as $key => $item) {
+            $personalLinks .= $this->makeListItem( $key, $item );
+        }
+
+        $personalTools = Html::rawElement("h5", [], "Nordlichter");
+        $personalTools .= Html::rawElement("ul", [], $personalLinks);
+
+        $sidebar = $titel.$form.$personalTools;
+        return Html::rawElement("div", ["class"=>"col-2 sidebar right"], $sidebar);
     }
 
     protected function getMyFooter()
     {
-        return "";
+        $list = "";
+        $footerlinks = array('lastmod', 'viewcount');
+        foreach( $footerlinks as $aLink ) {
+            if( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
+                $list .= Html::rawElement("li", [], $this->get($aLink));
+            }
+        }
+        $html = Html::rawElement("div", ["class" => "row"],
+            Html::rawElement("div", ["class" => "col"],
+                Html::rawElement("div", ["class" => "foot"],
+                    Html::rawElement("ul", [], $list))
+            )
+        );
+        return $html;
     }
 }
